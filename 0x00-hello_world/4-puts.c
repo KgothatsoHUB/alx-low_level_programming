92d9de905424a0f9f134668a423532b34bcaa23f
KgothatsoHUB <kgothikhoza@gmail.com>
2023-06-15 07:53:04 -0700

    C - Hello, World

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ *Main - MAin entry point
+ *return : always 0 (succed)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+/**
+ * Hello, printf 
+ * */
+int main (void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
