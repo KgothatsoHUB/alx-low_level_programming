9d15fbcf01d795225bcca79ef6fa82f430948843
KgothatsoHUB <kgothikhoza@gmail.com>
2023-06-29 06:01:45 -0700

    C - More pointers, arrays and strings

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -10,7 +10,7 @@ char *cap_string(char *str)
 {
 	int index = 0;
 
-	while (str[index])
+	while (str[++index])
 	{
 		while (!(str[index] >= 'a' && str[index] <= 'z'))
 			index++;
@@ -30,8 +30,6 @@ char *cap_string(char *str)
 		    str[index - 1] == '}' ||
 		    index == 0)
 			str[index] -= 32;
-
-		index++;
 	}
 
 	return (str);
