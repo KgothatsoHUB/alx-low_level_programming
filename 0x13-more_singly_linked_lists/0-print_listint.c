934f4461551298b14258e1bb0fc561337f3b45be
Kgothatso <60695763+KgothatsoHUB@users.noreply.github.com>
2023-08-03 21:28:02 +0200

    Update 0-print_listint.c

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -3,9 +3,8 @@
 /**
  * print_listint - Prints all the elements of a linked list.
  * @h: Linked list of type listint_t to print.
- *
  * Return: Num of nodes.
-*/
+**/
 
 size_t print_listint(const listint_t *h)
 {
