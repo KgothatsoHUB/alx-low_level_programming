f6b6a2c2cf93b23853a9c6854258ebe035f7b2f3
Kgothatso <60695763+KgothatsoHUB@users.noreply.github.com>
2023-08-03 21:31:33 +0200

    Update 100-reverse_listint.c

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -10,19 +10,20 @@
 listint_t *reverse_listint(listint_t **head)
 
 {
- listint_t *prev = NULL;
- listint_t *next = NULL;
-
- while (*head)
- {
+	listint_t *prev = NULL;
+	listint_t *next = NULL;
+	
+	while (*head)
+	
+{
  
-	 next = (*head)->next;
-	 (*head)->next = prev;
-	 prev = *head;
-	 *head = next;
- }
+	next = (*head)->next;
+	(*head)->next = prev;
+	prev = *head;
+	*head = next;
+}
 
- *head = prev;
+	*head = prev;
 
- return (*head);
+	return (*head);
 }
