110af9aeef4c4630defb8e4ca896b9305d0ff362
Kgothatso H. Khoza <Kgothikhoza@gmail.com>
2023-08-10 01:39:27 -0700

    Update

--- 0x13-more_singly_linked_lists/8-sum_listint.c
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -10,14 +10,17 @@
 int sum_listint(listint_t *head)
 
 {
- int sum = 0;
- listint_t *temp = head;
+	int sum = 0;
+	listint_t *temp = head;
 
- while (temp)
- {
- sum += temp->n;
- temp = temp->next;
- }
+	while (temp)
 
- return (sum);
+	{
+		sum += temp->n;
+		temp = temp->next;
+
+	}
+
+	return (sum);
+
 }
--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -13,35 +13,44 @@
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 
 {
- unsigned int i;
- listint_t *new;
- listint_t *temp = *head;
+	unsigned int i;
+	listint_t *new;
+	listint_t *temp = *head;
 
- new = malloc(sizeof(listint_t));
- if (!new || !head)
- return (NULL);
+	new = malloc(sizeof(listint_t));
 
- new->n = n;
- new->next = NULL;
+	if (!new || !head)
 
- if (idx == 0)
- {
- 	new->next = *head;
- 	*head = new;
- 	return (new);
- }
+		return (NULL);
+	new->n = n;
+	new->next = NULL;
 
- for (i = 0; temp && i < idx; i++)
- {
- 	if (i == idx - 1)
- 	{
- 	new->next = temp->next;
- 	temp->next = new;
- 	return (new);
- 	}
- 	else
- 	temp = temp->next;
- }
+	if (idx == 0)
+	{
 
- return (NULL);
+		new->next = *head;
+		*head = new;
+
+		return (new);
+
+	}
+
+	for (i = 0; temp && i < idx; i++)
+
+	{
+
+		if (i == idx - 1)
+
+		{
+			new->next = temp->next;
+			temp->next = new;
+			return (new);
+		}
+
+		else
+			temp = temp->next;
+	}
+
+	return (NULL);
+
 }
