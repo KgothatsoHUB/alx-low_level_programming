8400e82b37af38853df1cc8fcdf0481b1adbaf0b
Kgothatso H. Khoza <kgothikhoza@gmail.com>
2023-09-29 07:44:00 -0700

    update

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
  * set_bit - Sets a bit at a given index to 1.
  * @n: Pointer to the numb to change.
@@ -15,8 +16,9 @@ int set_bit(unsigned long int *n, unsigned int index)
 
 		return (-1);
 
-	*n = ((1UL << index) | *n);
+	*n = ((1UL << index) & *n);
 
 	return (1);
 
 }
+
