b8468a673d978ab3c37666de1d9fba4408938e1d
Kgothatso H. Khoza <kgothikhoza@gmail.com>
2023-10-04 06:34:31 -0700

    updated

--- 0x15-file_io/0-main.c
+++ 0x15-file_io/0-main.c
@@ -3,24 +3,36 @@
 #include "main.h"
 
 /**
- * main - check the code
- * Return: Always 0.
- **/
+ * main - Entry point of the program.
+ * @ac: The number of command-line arguments.
+ * @av: An array of strings representing the command-line arguments.
+ *
+ * Description:
+ * The main function serves as the entry point for the program. It checks
+ * if the correct number of command-line arguments is provided. If not,
+ * it prints a usage message to standard error and exits the program with
+ * a status code of 1. It then reads the specified filename using the
+ * read_textfile function with two different buffer sizes (114 and 1024),
+ * and prints the number of characters read for each size.
+ *
+ * Return:
+ * Always returns 0.
+ */
 
 int main(int ac, char **av)
 
 {
-    ssize_t n;
+	ssize_t n;
 
-    if (ac != 2)
-    {
-        dprintf(2, "Usage: %s filename\n", av[0]);
-        exit(1);
-    }
-    n = read_textfile(av[1], 114);
-    printf("\n(printed chars: %li)\n", n);
-    n = read_textfile(av[1], 1024);
-    printf("\n(printed chars: %li)\n", n);
-    return (0);
+	if (ac != 2)
+	{
+	dprintf(2, "Usage: %s filename\n", av[0]);
+	exit(1);
+	}
+	n = read_textfile(av[1], 114);
+	printf("\n(printed chars: %li)\n", n);
+	n = read_textfile(av[1], 1024);
+	printf("\n(printed chars: %li)\n", n);
+	return (0);
 }
 
